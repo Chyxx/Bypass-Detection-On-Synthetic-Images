f8aa865a2faa2814ee1e011c54fec398ba442d2a
cyx <1789757099@qq.com>
2023-10-13 14:59:09 +0800

    更改

--- config.py
+++ config.py
@@ -13,10 +13,7 @@ parser.add_argument("--delta", type=float, default=2e-3, help="step of estimated
 parser.add_argument("--epsilon", type=float, default=5e-3, help="variance of generated noise")
 parser.add_argument("--sigma", type=float, default=1.5e-3, help="std of extra noise")
 parser.add_argument("--num", type=int, default=20, help="number of try noise")
-parser.add_argument(
-    "-f", "--file", default="E:/data/imagenet_ai_0424_sdv5/train", type=str, help="path to data directory"
-)
-parser.add_argument("--arch", type=str, default="resnet50")
+parser.add_argument("-f", "--file", default="E:/data/imagenet_ai_0424_sdv5/train", type=str, help="path to data directory")
 parser.add_argument("--image_channels", type=int, default=3, help="number of image channels")
 parser.add_argument("--model_channels", type=int, default=64, help="number of model channels")
 parser.add_argument("--processor_path", type=str, default="data/processor-ckpt/train_noise/fixed_noise_v2_9_0_.pkl")
